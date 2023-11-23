4eb76d63af96449ba78c98afacfdc2fecfc412d1
HectorUIDE <hectormanuel.oramas@gmail.com>
2023-11-22 19:55:47 -0500

    Main y Books

--- book.go
+++ book.go
@@ -1,1 +1,31 @@
 package main
+
+import "fmt"
+
+type Book struct {
+	Title    string
+	Author   string
+	Category string
+}
+
+var Books []Book
+
+func AddBook() {
+	var newBook Book
+	fmt.Print("Enter Title: ")
+	fmt.Scanln(&newBook.Title)
+	fmt.Print("Enter Author: ")
+	fmt.Scanln(&newBook.Author)
+	fmt.Print("Enter Category: ")
+	fmt.Scanln(&newBook.Category)
+
+	Books = append(Books, newBook)
+	fmt.Println("Book added successfully!")
+}
+
+func ViewBooks() {
+	fmt.Println("Books:")
+	for _, book := range Books {
+		fmt.Printf("Title: %s, Author: %s, Category: %s\n", book.Title, book.Author, book.Category)
+	}
+}
--- main.go
+++ main.go
@@ -1,7 +1,46 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
-	fmt.Println("what in tarnation? ಠ_ಠ")
+	for {
+		fmt.Println("Electronic Book Management System")
+		fmt.Println("1. Add Book")
+		fmt.Println("2. View Books")
+		fmt.Println("3. Add Category")
+		fmt.Println("4. View Categories")
+		fmt.Println("5. Search Books")
+		fmt.Println("6. Save to File")
+		fmt.Println("7. Load from File")
+		fmt.Println("8. Exit")
+
+		var choice int
+		fmt.Print("Enter your choice: ")
+		fmt.Scanln(&choice)
+
+		switch choice {
+		case 1:
+			AddBook()
+		case 2:
+			ViewBooks()
+		case 3:
+			//			addCategory()
+		case 4:
+			//			viewCategories()
+		case 5:
+			//			searchMenu()
+		case 6:
+			//			saveToFile()
+		case 7:
+			//			loadFromFile()
+		case 8:
+			fmt.Println("Exiting program...")
+			os.Exit(0)
+		default:
+			fmt.Println("Invalid choice. Please try again.")
+		}
+	}
 }
